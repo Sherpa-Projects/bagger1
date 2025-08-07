d3a64b7861426fb27814ded0f529322e7f7b0f81
kevin <horn@jaimo-solutions.com>
2025-08-07 09:30:29 +0200

    update machine data page, props and data pool

--- src/app/[location]/[machine]/page.tsx
+++ src/app/[location]/[machine]/page.tsx
@@ -32,7 +32,7 @@ export default function MachinePage({
   return (
     <>
       <Navigation />
-      <main className="pb-16 pt-10 lgpt-30 px-4">
+      <main className="pb-16 pt-40 px-4">
         <div className="container mx-auto md:max-w-4xl lg:max-w-5xl xl:max-w-6xl">
           <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl pb-0 lg:pb-4 lg:leading-tight text-center">
             {selectedMachine?.name} in {currentLocation}
@@ -48,64 +48,136 @@ export default function MachinePage({
                   height={500}
                 />
               )}
-
-              <h2 className="text-3xl font-bold text-brown-800 mb-6">Preise</h2>
-              <ul className="border border-gray-200 text-lg">
-                <li className="bg-yellow-100 flex justify-between items-center p-4 rounded-md">
-                  <span>2–4 Stunden</span>
-                  <span className="font-bold">50 €*</span>
-                </li>
-                <li className="bg-white flex justify-between items-center p-4 rounded-md">
-                  <span>6–8 Stunden</span>
-                  <span className="font-bold">55 €*</span>
-                </li>
-                <li className="bg-yellow-100 flex justify-between items-center p-4 rounded-md">
-                  <span>10 Stunden</span>
-                  <span className="font-bold">65 €*</span>
-                </li>
-                <li className="bg-white flex justify-between items-center p-4 rounded-md">
-                  <span>
-                    1 Tag{" "}
-                    <span className="ml-1 text-sm text-black bg-neutral-200 rounded-full px-2 py-0.5">
-                      i
-                    </span>
-                  </span>
-                  <span className="font-bold">70 €*</span>
-                </li>
-                <li className="bg-yellow-100 flex justify-between items-center p-4 rounded-md">
-                  <span>
-                    5 Tage{" "}
-                    <span className="ml-1 text-sm text-black bg-neutral-200 rounded-full px-2 py-0.5">
-                      i
-                    </span>
-                  </span>
-                  <span className="font-bold">270 €*</span>
-                </li>
-                <li className="bg-white flex justify-between items-center p-4 rounded-md">
-                  <span>
-                    Woche{" "}
-                    <span className="ml-1 text-sm text-black bg-neutral-200 rounded-full px-2 py-0.5">
-                      i
-                    </span>
-                  </span>
-                  <span className="font-bold">330 €*</span>
-                </li>
-                <li className="bg-yellow-100 flex justify-between items-center p-4 rounded-md">
-                  <span>
-                    Wochenende{" "}
-                    <span className="ml-1 text-sm text-black bg-neutral-200 rounded-full px-2 py-0.5">
-                      i
-                    </span>
-                  </span>
-                  <span className="font-bold">165 €*</span>
-                </li>
-              </ul>
+              {selectedMachine?.content && (
+                <>
+                  <div>
+                    <h3 className="text-3xl font-bold mb-2">
+                      {selectedMachine.content.title}
+                    </h3>
+                    <p className="text-lg">
+                      {selectedMachine.content.description}
+                    </p>
+                  </div>
+                  <div className="mt-12">
+                    {selectedMachine.content.dataSheet && (
+                      <div className="text-lg space-y-4">
+                        {selectedMachine.content.dataSheet.dimensions && (
+                          <div>
+                            <h4 className="font-bold">Bemessung:</h4>
+                            <ul>
+                              {selectedMachine.content.dataSheet.dimensions
+                                .width && (
+                                <li>
+                                  Gesamtbreite:{" "}
+                                  {
+                                    selectedMachine.content.dataSheet.dimensions
+                                      .width
+                                  }
+                                </li>
+                              )}
+                              {selectedMachine.content.dataSheet.dimensions
+                                .length && (
+                                <li>
+                                  Gesamtlänge:{" "}
+                                  {
+                                    selectedMachine.content.dataSheet.dimensions
+                                      .length
+                                  }
+                                </li>
+                              )}
+                              {selectedMachine.content.dataSheet.dimensions
+                                .height && (
+                                <li>
+                                  Gesamthöhe:{" "}
+                                  {
+                                    selectedMachine.content.dataSheet.dimensions
+                                      .height
+                                  }
+                                </li>
+                              )}
+                              {selectedMachine.content.dataSheet.dimensions
+                                .weight && (
+                                <li>
+                                  Betriebsgewicht:{" "}
+                                  {
+                                    selectedMachine.content.dataSheet.dimensions
+                                      .weight
+                                  }
+                                </li>
+                              )}
+                              {selectedMachine.content.dataSheet.dimensions
+                                .volume && (
+                                <li>
+                                  Löffelvolumen:{" "}
+                                  {
+                                    selectedMachine.content.dataSheet.dimensions
+                                      .volume
+                                  }
+                                </li>
+                              )}
+                            </ul>
+                          </div>
+                        )}
+                        {selectedMachine.content.dataSheet.power && (
+                          <p>
+                            Leistung: {selectedMachine.content.dataSheet.power}
+                          </p>
+                        )}
+                        {selectedMachine.content.dataSheet.workingRange && (
+                          <div>
+                            <h4 className="font-bold">Arbeitsbereich:</h4>
+                            <ul>
+                              {selectedMachine.content.dataSheet.workingRange
+                                .maxDepth && (
+                                <li>
+                                  Max. Grabtiefe:{" "}
+                                  {
+                                    selectedMachine.content.dataSheet
+                                      .workingRange.maxDepth
+                                  }
+                                </li>
+                              )}
+                            </ul>
+                          </div>
+                        )}
+                      </div>
+                    )}
+                  </div>
+                </>
+              )}
+              <div></div>
             </div>
             <div className="order-1 lg:order-2 lg:col-span-1 flex justify-center rounded-xl p-6">
-              <rtr-article-booking
-                article-id="GXHBU7"
-                view="calendar"
-              ></rtr-article-booking>
+              <div className="flex flex-col space-y-12">
+                {selectedMachine?.articleId && (
+                  <rtr-article-booking
+                    article-id={selectedMachine?.articleId}
+                    view="calendar"
+                  ></rtr-article-booking>
+                )}
+
+                <h2 className="text-3xl font-bold mb-6">Preise</h2>
+                <ul className="border border-gray-200 text-lg">
+                  <li className="bg-yellow-50 flex justify-between items-center p-4 border-b border-gray-200 last:border-b-0">
+                    <span>1 Tag </span>
+                    <span className="font-bold">
+                      {selectedMachine?.pricePerDay} €
+                    </span>
+                  </li>
+                  <li className="bg-white flex justify-between items-center p-4 border-b border-gray-200 last:border-b-0">
+                    <span>Woche </span>
+                    <span className="font-bold">
+                      {selectedMachine?.pricePerWeek} €
+                    </span>
+                  </li>
+                  <li className="bg-yellow-50 flex justify-between items-center p-4 border-b border-gray-200 last:border-b-0">
+                    <span>Woche </span>
+                    <span className="font-bold">
+                      {selectedMachine?.pricePerMonth} €
+                    </span>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
--- src/app/types/machineDataProps.ts
+++ src/app/types/machineDataProps.ts
@@ -3,10 +3,37 @@ import { LocationSlug } from "./locationDataProps";
 export type MachineDataProps = {
   name: string;
   slug: string;
+  model: string;
+  category: string;
   image: {
     url: string;
     alt: string;
   };
   pricePerDay: number;
+  pricePerWeek?: number;
+  pricePerMonth?: number;
+  content?: {
+    title: string;
+    description: string;
+    dataSheet: {
+      dimensions?: {
+        width?: string;
+        length?: string;
+        height?: string;
+        weight?: string;
+        volume?: string;
+      };
+      power?: string;
+      workingRange?: {
+        maxDepth?: string;
+        maxReach?: string;
+        swingRadius?: string;
+        breakoutForce?: string;
+        tearOutForce?: string;
+      };
+      content?: string;
+    };
+  };
   availableAt: LocationSlug[];
+  articleId: string;
 };
