3ff48d0b5682c8ab99e4b9fc8b418fb596e55b5e
kevin <horn@jaimo-solutions.com>
2025-09-13 16:18:09 +0200

    change google map link to general locations

--- src/lib/content/locationData.ts
+++ src/lib/content/locationData.ts
@@ -12,7 +12,7 @@ export const locationData: LocationDataProps[] = [
       url: "/images/locations/mannheim.jpg",
       alt: "Luftaufnahme von Mannheim",
     },
-    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2590.830596765222!2d8.5438917!3d49.506592999999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4797cfae51c2864d%3A0xf56481b4867e43fe!2sWittenberger%20Weg%202%2C%2068309%20Mannheim!5e0!3m2!1sde!2sde!4v1755414127409!5m2!1sde!2sde",
+    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d165835.00660487727!2d8.337267677908613!3d49.50014623135492!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4797cc24518e3f45%3A0xb1e4fe7aa406e687!2sMannheim!5e0!3m2!1sde!2sde!4v1757770126030!5m2!1sde!2sde",
   },
   {
     name: "Hennef",
@@ -25,7 +25,7 @@ export const locationData: LocationDataProps[] = [
       url: "/images/locations/hennef.jpg",
       alt: "Luftaufnahme von Hennef",
     },
-    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2523.4791819336456!2d7.340086800000002!3d50.7666902!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bee9c417b1052b%3A0xf91bb74d40f94b14!2sKapellenh%C3%BCgel%203%2C%2053773%20Hennef%20(Sieg)!5e0!3m2!1sde!2sde!4v1755414081856!5m2!1sde!2sde",
+    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d379801.3642333542!2d7.135345617082178!3d50.80922951890131!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bee8056b47f5c5%3A0xf2cd3de7b6d82333!2s53773%20Hennef%20(Sieg)!5e0!3m2!1sde!2sde!4v1757770190281!5m2!1sde!2sde",
   },
   {
     name: "Bruchsal",
@@ -38,6 +38,6 @@ export const locationData: LocationDataProps[] = [
       url: "/images/locations/bruchsal.jpg",
       alt: "Bruchsaler Schloss",
     },
-    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2609.290784134524!2d8.5885657!3d49.157088599999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4797a388a52d9a95%3A0xcdda3373d406ccc6!2sSchlesierstra%C3%9Fe%205%2C%2076694%20Forst!5e0!3m2!1sde!2sde!4v1755414168158!5m2!1sde!2sde",
+    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d83542.85740850326!2d8.516518458803844!3d49.130061060922436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4797a15dd7998d39%3A0xc4a743128bd895c7!2sBruchsal!5e0!3m2!1sde!2sde!4v1757769980017!5m2!1sde!2sde",
   },
 ];
