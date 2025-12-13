1422f9c564e8d7a80516ee579afb35c80a426607
kevin <horn@jaimo-solutions.com>
2025-12-13 12:17:17 +0100

    set default og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,35 @@
-"use client";
-
 import "./globals.css";
+import type { Metadata } from "next";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 import Consent from "@/components/Consent";
 import ThirdPartyScripts from "@/components/ThirdPartyScripts";
+
+const OG_IMAGE_URL = "https://bagger1.de/images/og-image.png";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://bagger1.de"),
+  title: {
+    default: "Ihre Nummer 1 für Bagger und Maschinen | Bagger1",
+    template: "%s | Bagger1",
+  },
+  openGraph: {
+    siteName: "Bagger1",
+    images: [
+      {
+        url: OG_IMAGE_URL,
+        width: 1200,
+        height: 630,
+        alt: "Bagger1",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    images: [OG_IMAGE_URL],
+  },
+};
 
 export default function RootLayout({
   children,
