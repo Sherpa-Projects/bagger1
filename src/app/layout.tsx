d54fde0c7dcbb78809302b431e91c6d9bf290ce4
kevin <horn@jaimo-solutions.com>
2025-12-14 08:39:13 +0100

    link favicons explicit in layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,15 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     images: [OG_IMAGE_URL],
   },
+  icons: {
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
