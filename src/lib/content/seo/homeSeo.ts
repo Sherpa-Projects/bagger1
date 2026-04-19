f26a31c8d377c1e504dd571e7b5c44c0cd824105
kevin <horn@jaimo-solutions.com>
2026-04-19 10:21:37 +0200

    shift home page seo data in specific component

---
+++ src/lib/content/seo/homeSeo.ts
@@ -0,0 +1,18 @@
+export function getHomeSeoTexts() {
+  const title = "Ihre Nummer 1 für Bagger und Maschinen | BAGGER1";
+  const googleDescription =
+    "Verfügbare Bagger & Maschinen in Ihrer Nähe finden – flexibel mieten, transparent buchen und direkt online reservieren bei BAGGER1.";
+  const openGraphDescription =
+    "Finden Sie jetzt verfügbare Bagger und Maschinen an Ihrem Standort. Mit flexiblen Zeiträumen, transparenten Preisen und direkter Online-Buchung bei BAGGER1.";
+  const twitterDescription =
+    "Bagger & Maschinen in Ihrer Nähe – flexibel mieten & direkt online buchen bei BAGGER1.";
+  const alt = "Startseite | BAGGER1";
+
+  return {
+    title,
+    googleDescription,
+    openGraphDescription,
+    twitterDescription,
+    alt,
+  };
+}
