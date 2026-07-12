06f8dc6140cc48c37d0c807c62e9a12ffe194dbd
kevin <horn@jaimo-solutions.com>
2026-07-12 15:43:11 +0200

    add delivery areas for all locations  and json ld to location page

--- src/lib/content/seo/locationSeo.ts
+++ src/lib/content/seo/locationSeo.ts
@@ -1,4 +1,5 @@
 import { getCityName } from "@/lib/utils";
+import { LocationDataProps } from "@/app/types/Location";
 
 export function getLocationSeoTexts(location: string) {
   const cityName = getCityName(location);
@@ -16,5 +17,50 @@ export function getLocationSeoTexts(location: string) {
     openGraphDescription,
     twitterDescription,
     alt,
+  };
+}
+
+export function getLocationServiceJsonLd(location: LocationDataProps) {
+  const baseUrl = "https://bagger1.de";
+
+  return {
+    "@context": "https://schema.org",
+    "@type": "Service",
+    "@id": `${baseUrl}/${location.slug}/#rental-service`,
+
+    name: location.seo.title,
+    url: `${baseUrl}/${location.slug}`,
+    description: location.seo.description,
+
+    serviceType: "Vermietung von Baggern und Baumaschinen",
+
+    provider: {
+      "@id": `${baseUrl}/#organization`,
+    },
+
+    areaServed: location.deliveryAreas.map((area) => ({
+      "@type": "City",
+      name: area.city,
+      address: {
+        "@type": "PostalAddress",
+        postalCode: area.postalCode,
+        addressLocality: area.city,
+        addressCountry: "DE",
+      },
+    })),
+
+    ...(location.contact.telephone && {
+      availableChannel: {
+        "@type": "ServiceChannel",
+        serviceUrl: `${baseUrl}/${location.slug}`,
+        servicePhone: {
+          "@type": "ContactPoint",
+          telephone: location.contact.telephone,
+          email: location.contact.email,
+          contactType: "customer service",
+          availableLanguage: ["de"],
+        },
+      },
+    }),
   };
 }
